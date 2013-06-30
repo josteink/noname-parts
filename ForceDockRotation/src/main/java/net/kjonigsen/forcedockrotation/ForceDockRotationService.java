a842e8f3fac1a39e951861269e01635f621db1c5
Jostein Kjønigsen <jostein@kjonigsen.net>
2013-06-30 16:32:29 +0200

    Reenable orientation-forcing code. Declare appropriate permissions.

--- ForceDockRotation/src/main/java/net/kjonigsen/forcedockrotation/ForceDockRotationService.java
+++ ForceDockRotation/src/main/java/net/kjonigsen/forcedockrotation/ForceDockRotationService.java
@@ -16,15 +16,6 @@ import java.beans.IndexedPropertyChangeEvent;
 public class ForceDockRotationService extends Service {
     LinearLayout orientationChanger;
 
-    private final static int FORCED_SCREEN_ORIENTATION = 0x4;
-
-    /**
-    * Window type: system overlay windows, which need to be displayed
-    * On top of everything else. These windows must not take input
-    * Focus, or they will interfere with the keyguard.
-    */
-    public static final int TYPE_SYSTEM_OVERLAY = WindowManager.LayoutParams.FIRST_SYSTEM_WINDOW + 6;
-
     public IBinder onBind(Intent intent) {
         return null;
     }
@@ -33,14 +24,13 @@ public class ForceDockRotationService extends Service {
     public void onCreate() {
         orientationChanger = new LinearLayout(this);
 
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(TYPE_SYSTEM_OVERLAY, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.RGBA_8888);
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.RGBA_8888);
+        params.screenOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
 
-        params.screenOrientation = FORCED_SCREEN_ORIENTATION;
+        WindowManager window = (WindowManager)getSystemService("window");
+        window.addView(orientationChanger,params);
 
-//        WindowManager window = (WindowManager)getSystemService("window");
-//        window.addView(orientationChanger,params);
-//
-//        orientationChanger.setVisibility(0);
+        orientationChanger.setVisibility(0);
     }
 
     @Override
