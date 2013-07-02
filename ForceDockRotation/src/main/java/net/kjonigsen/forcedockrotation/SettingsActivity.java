6fdd71a72a402b20c3abdfbfec016edab19edf07
Jostein Kjønigsen <jostein@kjonigsen.net>
2013-07-02 21:07:47 +0200

    Make app instantly respond to config-changes.

--- ForceDockRotation/src/main/java/net/kjonigsen/forcedockrotation/SettingsActivity.java
+++ ForceDockRotation/src/main/java/net/kjonigsen/forcedockrotation/SettingsActivity.java
@@ -1,12 +1,36 @@
 package net.kjonigsen.forcedockrotation;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        preferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        preferences.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        DeviceStatus status = DeviceStatus.fromSystem(this);
+        DockRotationReceiver.startStopService(this, status.IsDocked);
     }
 }
